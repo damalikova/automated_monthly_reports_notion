e90e7fdbe1e7176e12d5f619aaf99f2d3eaf37cc
Daria Malikova <55386345+damalikova@users.noreply.github.com>
2023-06-17 17:07:06 +0300

    added filtration by month and sorting by date (from oldest to newest)

--- main.py
+++ main.py
@@ -5,21 +5,29 @@ from config import token, database_id
 url = f"https://api.notion.com/v1/databases/{database_id}/query"
 
 headers = {
-    "Authorization": f"Bearer {token}",
-    "accept": "application/json",
-    "Notion-Version": "2022-06-28"}
+            "Authorization": f"Bearer {token}",
+            "accept": "application/json",
+            "Notion-Version": "2022-06-28"
+          }
 
-payload = {
-           "filter": {
-               "property": "Date",
-               "date": {
-                   "after": "2023-06-15"
-               }
+payload = {'filter': {'and':
+                      [
+                        {'date':
+                         {'after': '2023-05-31'},
+                         'property': 'Date'},
+                        {'date':
+                         {'before': '2023-07-01'},
+                         'property': 'Date'}
+                      ]
+                      },
+           "sorts": [
+               {
+                   "property": "Date",
+                   "direction": "ascending"}
+           ]
            }
-        }
-
 
 response = requests.post(url, json=payload, headers=headers).json()
 
-print(len(response))
-pprint(response)
+pprint((response["results"][0]))
+# pprint(response["results"])
